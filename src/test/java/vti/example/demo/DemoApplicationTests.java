68b115f815cb1a687796211721a59db3e0f58a8d
DESKTOP-OIJ636C\phanhoainam <phanhoainam12g4@gmail.com>
2023-08-15 13:45:07 +0700

    quanlysinhvien1

---
+++ src/test/java/vti/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vti.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
